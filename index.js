f51339b4b47b810e88473dee8464c2d1d13d3fb9
Shawn Petros <arashcuzi@gmail.com>
2017-02-12 22:17:52 -0600

    added pointer to mLab and set up dockerfile and vagrantfile, toying with running this on my windows box

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var cmd = require('node-cmd');
+var mLab = require('mongolab-data-api')('TKVMJQX_MG2QTInjfPW6PJNw0oLuSbuP');
 
-cmd.get('speed-test --json', data => {
-  console.log(data);
-});
+var options = {
+  database: 'time-warner-speeds',
+  collectionName: 'results'
+};
+
+let getSpeed = () => {
+  cmd.get('speed-test --json', data => {
+    options.documents = JSON.parse(data);
+
+    mLab.insertDocuments(options, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  });
+};
+
+getSpeed();
+
+setInterval(getSpeed, 30000);
